1a217d66ff1d5fea4410accf62db9a3c25d16bb6
krisbitney <kristoferbitney@hotmail.com>
2023-05-02 13:57:05 +0100

    added tests for client methods `getFile` and `getManifest` (not passing)

---
+++ src/commonTest/kotlin/client/GetFileManifestTest.kt
@@ -0,0 +1,42 @@
+package client
+
+import io.polywrap.client.PolywrapClient
+import io.polywrap.configBuilder.ClientConfigBuilder
+import io.polywrap.core.resolution.Uri
+import io.polywrap.core.resolution.UriPackageOrWrapper
+import io.polywrap.core.wrap.WrapManifest
+import kotlin.test.Test
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+
+class GetFileManifestTest {
+
+    private val sha3Uri = Uri("ipfs/QmThRxFfr7Hj9Mq6WmcGXjkRrgqMG3oD93SLX27tinQWy5")
+    private val config = ClientConfigBuilder().addDefaults().build()
+    private val client = PolywrapClient(config)
+
+    @Test
+    fun tryResolveUriToPackage() {
+        val result = client.tryResolveUri(uri = sha3Uri, resolveToPackage = true)
+        assertNull(result.exceptionOrNull())
+        assertTrue(result.getOrThrow() is UriPackageOrWrapper.PackageValue)
+    }
+
+    @Test
+    fun getFile() {
+        val result = client.getFile(sha3Uri, "wrap.info")
+        assertNull(result.exceptionOrNull())
+
+        val manifest = WrapManifest.deserialize(result.getOrThrow())
+        assertNull(manifest.exceptionOrNull())
+
+        assertTrue(manifest.getOrThrow().name == "sha3wrapper")
+    }
+
+    @Test
+    fun getManifest() {
+        val manifest = client.getManifest(sha3Uri)
+        assertNull(manifest.exceptionOrNull())
+        assertTrue(manifest.getOrThrow().name == "sha3wrapper")
+    }
+}

b2ee32f05f6b8212e2052e2553be7d823d8cac12
krisbitney <kristoferbitney@hotmail.com>
2023-06-27 09:30:21 -0500

    added doc site to git

---
+++ docs/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":dokkaHtml/androidDebug":[":dokkaHtml/commonMain"],":dokkaHtml/androidMain":[":dokkaHtml/commonMain"],":dokkaHtml/androidRelease":[":dokkaHtml/commonMain"],":dokkaHtml/commonMain":[],":dokkaHtml/jvmMain":[":dokkaHtml/commonMain"]}'
